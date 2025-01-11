b7ebd6f30cda6ac146aae28c15d977f2bf68ea01
Liang Sun <sunly917@gmail.com>
2025-01-11 13:24:34 +0200

    add nvim config

---
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,9 @@
+return {
+  "akinsho/bufferline.nvim",
+  opts = {
+    options = {
+      show_close_icon = false, -- Hides the close icon in the bufferline
+      show_buffer_close_icons = false, -- Hides the close icons for individual buffers
+    },
+  },
+}
