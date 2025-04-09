afb95c4202b26e34d2467af270abcf3fd789986b
Liang Sun <sunly917@gmail.com>
2025-04-09 08:23:55 +0300

    feat: add support to svelte language

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -2,5 +2,6 @@ return {
   "williamboman/mason.nvim",
   opts = function(_, opts)
     table.insert(opts.ensure_installed, "black")
+    table.insert(opts.ensure_installed, "svelte-language-server")
   end,
 }
