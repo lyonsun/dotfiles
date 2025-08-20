2c03756feb6ae13a236ff6bded9e5c7fddf99fe5
Liang Sun <sunly917@gmail.com>
2025-08-20 10:01:15 +0300

    update lazyvim keymaps

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,9 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.set("n", "<leader>t", ":terminal<CR>")
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+
 vim.keymap.set("n", "<leader>gj", "<cmd>GoTagAdd json<cr>")
-vim.keymap.set("n", "<leader>pu", "<cmd>-30<cr>")
-vim.keymap.set("n", "<leader>pd", "<cmd>+30<cr>")
+
 vim.keymap.set("n", "<leader>hp", "<cmd>Gitsigns preview_hunk<cr>")
 vim.keymap.set("n", "<leader>hd", "<cmd>Gitsigns diffthis<cr>")
 vim.keymap.set("n", "<leader>hb", "<cmd>Gitsigns blame_line<cr>")
