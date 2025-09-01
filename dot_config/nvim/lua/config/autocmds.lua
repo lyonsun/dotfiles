9a8eeef554aff7ad9f470dae2e8a4e04af8453ef
Liang Sun <sunly917@gmail.com>
2025-09-01 21:48:06 +0300

    add auto command to use tab indentation in make files

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile", "BufEnter" }, {
+  pattern = { "Makefile", "makefile", "GNUmakefile", "*.mk", "*.mak" },
+  callback = function()
+    vim.bo.filetype = "make" -- Explicitly set filetype
+    vim.bo.expandtab = false
+    vim.bo.tabstop = 4
+    vim.bo.shiftwidth = 4
+    vim.bo.softtabstop = 4
+  end,
+})
